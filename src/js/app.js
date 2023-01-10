22d83e6ddf06c0e3198dd07e23e13e0b80c05b7f
Yosif Georgiev <iodesign@live.com>
2023-01-10 18:13:56 +0200

    NEW: adding JS filter

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
 import "../scss/app.scss";
-const R = require("ramda");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const evenNumbers = numbersArray.filter((number) => {
+    return number % 2 === 0;
+  });
+
+
+  evenNumbers.forEach((evenNum) => {
+   const li = document.createElement('li');
+    li.textContent = evenNum;
+    ul.appendChild(li);
+});
+
 });
